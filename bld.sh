87b64eac23fd2747e729697d818652c306ef9470
Danielo Rodriguez <rdanielo@gmail.com>
2016-08-20 17:50:27 +0200

    Build process updated Now is more clear about Noteself Deployment and online version deployment

--- bld.sh
+++ bld.sh
@@ -3,13 +3,13 @@
 # Build SamplePlugin for TiddlyWiki5
 
 tiddlywiki \
-	./*wiki \
+	./wiki \
 	--verbose \
 	--build \
 	|| exit 1
 
-mv *wiki/output/readme.md readme.md
-mv *wiki/output/license.md license.md
-mv *wiki/output/index.html ../gh-pages
+mv ./wiki/output/readme.md readme.md
+mv ./wiki/output/license.md license.md
+mv ./wiki/output/index.html ../gh-pages
 
 cd ../gh-pages && git commit -am "$1"
