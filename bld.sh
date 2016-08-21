635248204b9939a2b110d98e54ed9055dfacd5a8
Danielo Rodriguez <rdanielo@gmail.com>
2016-08-21 02:06:31 +0200

    Fixes to the build process and save filters

--- bld.sh
+++ bld.sh
@@ -2,7 +2,7 @@
 
 # Build SamplePlugin for TiddlyWiki5
 
-tiddlywiki \
+TIDDLYWIKI_PLUGIN_PATH="/h/My Dropbox/tiddlywiki/plugins/danielo515/tiddlypouchPlugin/master/tiddlypouchwiki/plugins" tiddlywiki \
 	./wiki \
 	--verbose \
 	--build \
@@ -11,5 +11,6 @@ tiddlywiki \
 mv ./wiki/output/readme.md readme.md
 mv ./wiki/output/license.md license.md
 mv ./wiki/output/index.html ../gh-pages
+mv ./wiki/output/online.html ../gh-pages/online/index.html
 
-cd ../gh-pages && git commit -am "$1"
+cd ../gh-pages && git commit -am "$1" && git push origin master
