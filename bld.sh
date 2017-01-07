ae4555acef6fe77d3dbb358d43b6ebdd327df576
Danielo Rodriguez <rdanielo@gmail.com>
2017-01-07 01:53:55 +0100

    - Fixed problems with plugin disable - Added service worker to online template - Build uses distribution folder

--- bld.sh
+++ bld.sh
@@ -2,7 +2,7 @@
 
 # Build SamplePlugin for TiddlyWiki5
 
-TIDDLYWIKI_PLUGIN_PATH="/h/My Dropbox/tiddlywiki/plugins/danielo515/tiddlypouchPlugin/master/src/plugins" tiddlywiki \
+TIDDLYWIKI_PLUGIN_PATH="/h/My Dropbox/tiddlywiki/plugins/danielo515/tiddlypouchPlugin/master/dist" tiddlywiki \
 	./wiki \
 	--verbose \
 	--build index --build OnlineDemo \
@@ -13,4 +13,4 @@ mv ./wiki/output/license.md license.md
 mv ./wiki/output/index.html ../gh-pages
 mv ./wiki/output/online.html ../gh-pages/online/index.html
 
-cd ../gh-pages && git add . && git commit -m "$1" && git push origin master
+cd ../gh-pages && git add . && git commit -m "${1:-Deployed to github pages}" && git push origin master
