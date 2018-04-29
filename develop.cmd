0d6e3e7763890fe7e2c38d749e192f4f81fe879d
Danielo Rodriguez <rdanielo@gmail.com>
2018-04-29 10:59:58 +0200

    feat(dev): added package scripts to replace sh and cmd

--- develop.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-rem develop.cmd [<username>] [<password>]
-rem
-rem Serves development TW5 over HTTP at localhost:8080
-rem
-tiddlywiki.cmd ^
-	.\ ^
-	--verbose ^
-	--server 8092 $:/core/save/all text/plain text/html %1 %2
