d9229c604df1a93512fcdca26b550072a276ad92
Danielo Rodriguez <rdanielo@gmail.com>
2018-05-02 11:18:01 +0200

    feat(login): safer remote config set

--- src/core/login.js
+++ src/core/login.js
@@ -29,6 +29,30 @@ const namespace = (prefix) => (fn) => (x, ...args) => fn(prefix + x, ...args);
 const stateNamespace = namespace('$:/state/ns/');
 const setTexState = stateNamespace(setText);
 const validEmail = (email) => (/\w+@\w+\.\w{2,4}/).test(email)
+/**
+ * @function updateRemoteConfig
+ * Checks if the global tiddlypouch object exists. 
+ * If so it updates the current db configuration to store the API credentials
+ * Note that we are using promises as Either (Left Right -> reject resolve)
+ * @param  {Object} options          Object containing the required fields
+ * @param  {Object} options.db       Remote database name
+ * @param  {Object} options.host     Hostname where the remote database is
+ * @param  {Object} options.key      The username of the API key
+ * @param  {Object} options.password The password of the API key
+ * @return {Promise} Resolves to a dbConfig of the updated config or rejects to an error message
+ */
+const updateRemoteConfig = ({ db, host, key, password }) => {
+
+    return (!$TPouch) 
+    ? Promise.reject('TiddlyPouch is not installed!!! It is a mandatory dependency')
+    : Promise.resolve(
+        $TPouch.config.updateRemoteConfig({
+            name: db,
+            url: 'https://' + host,
+            username: key,
+            password
+        }))
+}
 
 /**
  * @function markInvalidField
@@ -62,10 +86,10 @@ const validatePin = (pin, correlation_id) => {
         .post(backendUrl + '/api/login', {
             pin, correlation_id
         })
-        .then(({ data }) => {
-            setTexState('credentials', JSON.stringify(data));
+        .then(({ data }) => updateRemoteConfig(data) )
+        .catch((error) => {
+            setTexState('waiting-pin', 'no')
         })
-        .catch(() => setTexState('waiting-pin', 'no'))
 }
 
 
@@ -74,6 +98,8 @@ const validatePin = (pin, correlation_id) => {
  */
 exports.startup = () => {
 
+    // ===== EVENT HANDLERS =====
+    // This events arises on the UI and are dispatched to the internal methods
     $tw.rootWidget.addEventListener("tm-get-pin",
         ({ param: email }) => {
             console.log('Trying to get a pin', email);
