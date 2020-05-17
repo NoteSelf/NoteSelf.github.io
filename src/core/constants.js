a55a3b01b00b349bd60de993777a018c339991ed
Danielo Rodriguez <rdanielo@gmail.com>
2020-05-17 18:20:32 +0200

    feat: fix logout flow

--- src/core/constants.js
+++ src/core/constants.js
@@ -14,13 +14,17 @@ Constants used to name events and config tiddlers similar stuff
 
 'use strict';
 
+const {SYNC_STATE} = require('$:/plugins/danielo515/tiddlypouch/constants')
+
 module.exports = {
       // used on the UI of config couch database. It has a different constant name on tiddlypouch
       // TODO: make a function on tpouch side to get this info
      COUCH_CONFIG: '$:/plugins/danielo515/tiddlypouch/config/selected_database'
      , BACKEND_URL: "@@process.env.NOTESELF_BACKEND_URL"
+     , SYNC_STATE
      // ----- UI Events
      , GET_PIN: "tm-get-pin"
      , VALIDATE_PIN: "tm-validate-pin"
      , CUSTOM_LOGIN: "tm-custom-login"
+     , LOGOUT: "tm-NS-logout"
 };
